0e4b9ffbaf0504e2948b5c17c515c9ccbdab3d5b
root <root@mainframe.local>
2022-12-20 11:56:43 +0200

    started with some basic files as taken from other projects

---
+++ main_function.py
@@ -0,0 +1,26 @@
+from libs.rss_reader import send_to_telegram
+from libs.rss_fetch import get_rss_feed
+from libs.settings import twitch_channel
+import time
+import os
+
+# call the functions to get twtich videos
+get_rss_feed(twitch_channel)
+
+# send the URLs to telegram
+take_file = open('libs/urls.rss' , 'r')
+# print(take_file.readlines())
+for i in take_file.readlines():
+    # print(i)
+    send_to_telegram('/VideoDownloadBot '+i)
+    # time.sleep(1)
+
+# remove the RSS files 
+if os.path.exists("libs/feed.rss"):
+  os.remove("libs/feed.rss")
+else:
+  print("The file does not exist")
+if os.path.exists("libs/urls.rss"):
+  os.remove("libs/urls.rss")
+else:
+  print("The file does not exist")
